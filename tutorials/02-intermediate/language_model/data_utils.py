77738e8168d1cb18b70dd6451a66aa3ad7c6857c
nyxjemk <32337316+nyxjemk@users.noreply.github.com>
2018-02-17 22:38:14 +0100

    Update data_utils.py

--- tutorials/02-intermediate/language_model/data_utils.py
+++ tutorials/02-intermediate/language_model/data_utils.py
@@ -2,6 +2,12 @@ import torch
 import os
 
 class Dictionary(object):
+    """
+        Dictionary class provides a
+        word-to-id dictionary as well as 
+        a id-to-word dictionary 
+        for words added with add_word()
+    """
     def __init__(self):
         self.word2idx = {}
         self.idx2word = {}
@@ -25,22 +31,34 @@ class Corpus(object):
     def get_data(self, path, batch_size=20):
         # Add words to the dictionary
         with open(path, 'r') as f:
+            # number of tokens in corpus
             tokens = 0
+            # iterating through lines
             for line in f:
+                #getting words in line and adding '<eos>' tag
                 words = line.split() + ['<eos>']
+                # summing up tokens
                 tokens += len(words)
                 for word in words: 
+                    # adding words to dictionary
                     self.dictionary.add_word(word)  
         
         # Tokenize the file content
+        # text converted to ids as integers tensor
         ids = torch.LongTensor(tokens)
         token = 0
+        # opening file again
         with open(path, 'r') as f:
             for line in f:
                 words = line.split() + ['<eos>']
                 for word in words:
+                    # adding line as int to ids
                     ids[token] = self.dictionary.word2idx[word]
                     token += 1
+        # ids.size(0) = num of tokens
+        # calculating number of batches
         num_batches = ids.size(0) // batch_size
+        # deleting incomplete last batch
         ids = ids[:num_batches*batch_size]
+        # reshaping for batches
         return ids.view(batch_size, -1)
